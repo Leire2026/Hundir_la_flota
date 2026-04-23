005a83efda5ddd054f14c2bfb37bada44a53f705
Leire2026 <learrie@hotmail.com>
2026-04-23 10:16:55 +0200

    HLF_Version23_04_26

---
+++ main.py
@@ -0,0 +1,10 @@
+from Mi_trabajo.Python.hlf_Leire2.Hundir_la_flota.utils import *
+
+print("Bienvenido. Vamos a jugar una partida de Hundir la Flota. ¡Adelante!")
+
+BARCOS = [4]
+
+
+#if __name__ == "__main__":
+juego()
+
